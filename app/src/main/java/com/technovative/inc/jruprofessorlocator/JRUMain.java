eddf5b022ea68830a7685cf022013108ce2657ea
jccultima123 <johncyrillcorsanes@gmail.com>
2015-08-22 01:20:33 +0800

    Ready for testing

--- app/src/main/java/com/technovative/inc/jruprofessorlocator/JRUMain.java
+++ app/src/main/java/com/technovative/inc/jruprofessorlocator/JRUMain.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class JRUStudentMain extends AppCompatActivity {
+public class JRUMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jrustudent_main);
+        setContentView(R.layout.activity_jrumain);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_jrustudent_main, menu);
+        getMenuInflater().inflate(R.menu.menu_jrumain, menu);
         return true;
     }
 
